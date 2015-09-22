7475f56672dc15ac9c153e9654f4ae0a7a786a1a
JunHu <hujun09012@gmail.com>
2015-09-22 17:46:30 +0800

    rebuild template structure

--- purchasing/views.py
+++ purchasing/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def purchasingFollowingViews(request):
     context={}
     return render(request,"purchasing/purchasing_following.html",context)
+
+
+def pendingOrderViews(request):
+    context = {}
+    return render(request, "purchasing/pending_order.html", context)
