31663140e7cefb1f51d18c6bd22dc7ec77f2b6d8
DutBright <757289247@qq.com>
2015-09-28 17:12:29 +0800

    ad

--- purchasing/models.py
+++ purchasing/models.py
@@ -19,14 +19,14 @@ class BidForm(models.Model):
 
 class bidApply(models.Model):
     apply_id = models.CharField(unique=True, max_length=20, blank=False, verbose_name=u"标单申请编号")
-    apply_company = models.CharField(null=True, verbose_name=u"申请单位")
-    demand_company = models.CharField(null=True, verbose_name=u"需求单位")
+    apply_company = models.CharField(null=True, max_length=40, verbose_name=u"申请单位")
+    demand_company = models.CharField(null=True, max_length=40, verbose_name=u"需求单位")
     work_order = models.ForeignKey(BidFormStatus,null=False,verbose_name=u"工作令")
-    amount = models.IntegerField(defaults=0, verbose_name=u"数量")
-    bid_project = models.CharField(null=True, verbose_name=u"拟招(议)项目")
-    bid_date = models.DateTimeField(null=True,verbose_name=u"拟招(议)标时间")
-    special_model = models.CharField(null=True, verbose_name=u"规格、型号")
-    core_part = models.BooleanField(defaults=False, verbose_name="是否为核心件")
+    amount = models.IntegerField(verbose_name=u"数量")
+    bid_project = models.CharField(null=True, max_length=40, verbose_name=u"拟招(议)项目")
+    bid_date = models.DateTimeField(null=True, verbose_name=u"拟招(议)标时间")
+    special_model = models.CharField(null=True, max_length=40, verbose_name=u"规格、型号")
+    core_part = models.BooleanField(verbose_name="是否为核心件")
 
     class Meta:
         verbose_name = u"标单申请表"
@@ -36,17 +36,17 @@ class bidApply(models.Model):
 
 class qualityPriceCard(models.Model):
     apply_id = models.CharField(unique=True, max_length=20, blank=False, verbose_name=u"标单申请编号")
-    apply_company = models.CharField(null=True, verbose_name=u"申请单位")
-    demand_company = models.CharField(null=True, verbose_name=u"需求单位")
+    apply_company = models.CharField(null=True, max_length=40, verbose_name=u"申请单位")
+    demand_company = models.CharField(null=True, max_length=40, verbose_name=u"需求单位")
     work_order = models.ForeignKey(BidFormStatus,null=False,verbose_name=u"工作令")
-    amount = models.IntegerField(defaults=0, verbose_name=u"数量")
-    unit = models.CharField(null=True, verbose_name=u"单位")
-    content = models.CharField(null=True, verbose_name=u"内容")
-    material = models.CharField(null=True, verbose_name=u"材质")
-    delivery_period = models.CharField(null=True, verbose_name=u"交货期")
-    price = models.CharField(null=True, verbose_name=u"价格")
-    ability = models.CharField(null=True, verbose_name=u"厂家协作能力质量情况及业绩")
-    delivery_condition = models.CharField(null=True, verbose_name=u"交货及支付条件")
+    amount = models.IntegerField(verbose_name=u"数量")
+    unit = models.CharField(null=True, max_length=40, verbose_name=u"单位")
+    content = models.CharField(null=True, max_length=40, verbose_name=u"内容")
+    material = models.CharField(null=True, max_length=40, verbose_name=u"材质")
+    delivery_period = models.CharField(null=True, max_length=40, verbose_name=u"交货期")
+    price = models.CharField(null=True, max_length=40, verbose_name=u"价格")
+    ability = models.CharField(null=True, max_length=40, verbose_name=u"厂家协作能力质量情况及业绩")
+    delivery_condition = models.CharField(null=True, max_length=40, verbose_name=u"交货及支付条件")
     class Meta:
         verbose_name = u"标单申请表"
 
@@ -76,7 +76,7 @@ class SupplierFile(models.Model):
         verbose_name_plural = "文件上传"
 
     def __unicode__(self):
-        return self.name 
+        return self.name
 
 class ArrivalInspection(models.Model):
     material_confirm = models.BooleanField(null=False,default=False,verbose_name=u"实物确认")
