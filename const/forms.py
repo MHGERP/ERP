f49c902f616682f5c9de698dcf3c1f1c6dedf5d6
JunHu <hujun09012@gmail.com>
2015-09-30 10:40:11 +0800

    add form

--- const/forms.py
+++ const/forms.py
@@ -9,6 +9,6 @@ class InventoryTypeForm(forms.Form):
     JunHU
     summary: store all type of source inventory list
     """
-    inventoryType = forms.ChoiceField(required = True, label = u"明细清单", choices = INVENTORY_TYPE, widget = forms.Select(attrs = {'class': 'input form-control'}))
+    name = forms.ChoiceField(required = True, label = u"明细清单", choices = INVENTORY_TYPE, widget = forms.Select(attrs = {'class': 'input form-control'}))
 
 
