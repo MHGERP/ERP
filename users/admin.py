1db043bf06b0f4677303eeed376f0bfd4a88a5d4
JunHu <hujun09012@gmail.com>
2015-11-08 15:11:30 +0800

    add user_info table

--- users/admin.py
+++ users/admin.py
@@ -2,14 +2,14 @@
 # coding=utf-8
 
 from django.contrib import admin
-from users.models  import *
+from users.models  import Title, Group, Authority, SuperAdmin, UserInfo
 
 RegisterClass = (
     Title,
     Group,
     Authority,
     SuperAdmin,
-
+    UserInfo,
 )
 
 for item in RegisterClass:
