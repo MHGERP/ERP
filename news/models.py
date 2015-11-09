fab0409ee67362bd3a6212b67b27670a827f78ff
obelisk-the-mxl <mxl.obelisk@hotmail.com>
2015-11-09 22:53:31 +0800

    commit

--- news/models.py
+++ news/models.py
@@ -23,15 +23,15 @@ class NewsCategory(models.Model):
 
 
 class News(models.Model):
-	news_title = models.CharField(verbose_name = u"标题", blank=True, max_length=200)
-	news_content = models.TextField(verbose_name = u"新闻内容", blank=True)
-	news_date = models.DateField(verbose_name = u"发表时间", default=datetime.datetime.today, blank=True)
-	news_category = models.ForeignKey(NewsCategory, verbose_name = u"新闻类型", blank=True, null=True)
+    news_title = models.CharField(verbose_name = u"标题", blank=True, max_length=200)
+    news_content = models.TextField(verbose_name = u"新闻内容", blank=True)
+    news_date = models.DateField(verbose_name = u"发表时间", default=datetime.datetime.today, blank=True)
+    news_category = models.ForeignKey(NewsCategory, verbose_name = u"新闻类型", blank=True, null=True)
 
-	def __unicode__(self):
+    def __unicode__(self):
 		return self.news_title
 
-	class Meta:
+    class Meta:
 	    verbose_name = u"新闻"
 	    verbose_name_plural = u"新闻"
 
