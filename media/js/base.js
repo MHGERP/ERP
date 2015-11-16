3919bad278a11288eebbe11e0dc668834b4a86a0
chenmenghui <root@ubuntu.(none)>
2015-11-16 19:12:25 +0800

    userInfo

--- media/js/base.js
+++ media/js/base.js
@@ -286,20 +286,3 @@ $(".mynavbar a").each(function(){
         }
 });
 
-$("#update-user").click(function(){
-    Dajaxice.home.updateUserInfo(updateUserInfoCallback,{
-        "user_form":$("#user_info_form").serialize(true),
-    });
-});
-
-function updateUserInfoCallback(data){
-    alert(data);
-}
-
-$("#user-info").click(function(){
-    Dajaxice.home.getUserInfoForm(getUserInfoFormCallback,{});
-})
-
-function getUserInfoFormCallback(data){
-    $(".modal-body").html(data); 
-}
