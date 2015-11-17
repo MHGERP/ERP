a904371c6bbc523e23ce9094db352bac37eb678f
chousan1989 <347096491@qq.com>
2015-11-17 23:27:40 +0800

    base

--- production/views.py
+++ production/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+# coding: UTF-8
+from django.shortcuts import render
+
+def taskAllocationViews(request):
+    context={}
+    return render(request,"production/task_allocation.html",context)
