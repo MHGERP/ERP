31099465f52ac6c0c5d7fd9667d2249c4ed6efd1
DutBright <757289247@qq.com>
2015-11-17 14:39:27 +0800

    add the urls

---
+++ production/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+from django.conf.urls.defaults import *
+
+urlpatterns=patterns('',
+
+)
