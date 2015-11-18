9ccce34ae2e26289bb4282e55d5cc3c9f0d76e06
chousan1989 <347096491@qq.com>
2015-11-18 23:28:22 +0800

    d

--- production/views.py
+++ production/views.py
@@ -1,6 +1,10 @@
 # coding: UTF-8
 from django.shortcuts import render
+from production.forms import *
 
 def taskAllocationViews(request):
-    context={}
+    context={
+       # "taskallocationform":TaskAllocationSearchForm()
+    }
+
     return render(request,"production/task_allocation.html",context)
