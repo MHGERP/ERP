3ec79acb3928048bc9d40a3da7202a5f444b6c90
JunHu <hujun09012@gmail.com>
2015-12-22 15:45:38 +0800

    add processBOM

---
+++ media/js/techdata/processBOM.js
@@ -0,0 +1,9 @@
+$(document).ready(refresh);
+$("#order_search").click(refresh);
+function refresh(){
+    var id_work_order = $("#id_work_order").val();
+    Dajaxice.techdata.getProcessBOM(refreshCallBack, {"id_work_order": id_work_order, });
+}
+function refreshCallBack(data) {
+    $("#widget-box").html(data);
+}
