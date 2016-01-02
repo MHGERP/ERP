81d3787d4b5869ff887f2a6406b309796ab91465
JunHu <hujun09012@gmail.com>
2016-01-02 11:41:38 +0800

    merge & weld_seam_full_card

--- media/js/techdata/weld_list.js
+++ media/js/techdata/weld_list.js
@@ -6,5 +6,17 @@ function refresh() {
     Dajaxice.techdata.getWeldSeamList(refreshCallBack, {"id_work_order": id_work_order, });
 }
 function refreshCallBack(data) {
-    $("#widget-content").html(data);
+    $("#widget-box").html(data);
+}
+$(document).on("click", ".tr_materiel", function() {
+    var iid = $(this).attr("iid");
+    fill(iid);
+});
+
+function fill(iid) {
+    $("#card_modal").attr("iid", iid);
+    Dajaxice.techdata.getWeldSeamCard(getCardCallBack, {"full": true, "iid": iid});
+}
+function getCardCallBack(data) {
+    $("#weld_seam_card").html(data);
 }
