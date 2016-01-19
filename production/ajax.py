b2509af8374a113723240f17190e4a115b745fb0
wanglei-0707 <806935949@qq.com>
2016-01-19 09:53:51 +0800

    synthesize

--- production/ajax.py
+++ production/ajax.py
@@ -7,16 +7,30 @@ from dajaxice.decorators import dajaxice_register
 from dajaxice.utils import deserialize_form
 from django.utils import simplejson
 from const.models import WorkOrder
+from production.models import SynthesizeFileListStatus
 
 @dajaxice_register
 def getFileList(request, id_work_order):
-	"""
-	Lei
-	"""
-	workorder = WorkOrder.objects.get(id = id_work_order)
-	print workorder.order_index
-	context = {
-	    "workorder":workorder,
-	}
-	html = render_to_string("production/synthesize_detail_filelist.html",context)
-	return html
+    """
+    Lei
+    """
+    workorder = SynthesizeFileListStatus.objects.filter(workorder_id__id = id_work_order)
+    context = {
+        "workorder":workorder,
+    }
+    html = render_to_string("production/synthesize_detail_filelist.html",context)
+    return html
+
+@dajaxice_register
+def changeFileList(request, id, workorder_id):
+    """
+    Lei
+    """
+    workorder = SynthesizeFileListStatus.objects.filter(workorder_id__order_index = workorder_id)[0]
+    exec("workorder.%s=True"%id)
+    workorder.save()
+    context = {
+        "workorder":[workorder],
+    }
+    html = render_to_string("production/synthesize_detail_filelist.html",context)
+    return html
