103fd184e45bf956f6cc88a15e9e3788679ec829
wanglei-0707 <806935949@qq.com>
2016-01-20 14:52:49 +0800

    man-hour

--- media/js/production/man_hour_summarize.js
+++
@@ -1,6 +0,0 @@
-$(document).on("click","#hour-summarize-btn",function(){
-	$("#hour_summarize_modal").modal();
-})
-$(document).on("click","#hour-detail-btn",function(){
-	$("#hour_detail_modal").modal();
-})
--- script/1.py
+++ script/1.py
@@ -3,9 +3,22 @@ from django.shortcuts import render
 from django.db.models import Q
 from purchasing.models import *
 from django.contrib.auth.models import User
+from techdata.models import Processing
 
-a = ContractDetail()
-a.user = User.objects.get(username="123")
-a.amount = 0
-a.bidform = BidForm.objects.get(bid_id="111")
-a.save()
+# a = ContractDetail()
+# a.user = User.objects.get(username="123")
+# a.amount = 0
+# a.bidform = BidForm.objects.get(bid_id="111")
+# a.save()
+
+items_all = Processing.objects.all()
+for item in items_all:
+    print item.operate_date
+
+items = Processing.objects.dates('operate_date', 'month').distinct()
+
+for item in items:
+    print "%s-%s"%(item.year,item.month)
+
+
+
