8f61e638adc9cb36907044acc28adc7f5af3a196
DutBright <757289247@qq.com>
2016-03-09 15:50:52 +0800

    add the production

--- storage/models.py
+++ storage/models.py
@@ -160,10 +160,8 @@ class ApplyCardItemBase(models.Model):
 class WeldingMaterialApplyCard(models.Model):
     department=models.IntegerField(verbose_name=u'领用单位',choices=STORAGEDEPARTMENT_CHOICES,max_length=20,blank=False)
     index=models.IntegerField(verbose_name=u'编号',blank=False,unique=True)
-    create_time=models.DateField(verbose_name=u'填写时间',auto_now_add=True)
     workorder=models.ForeignKey(WorkOrder,verbose_name=u'工作令',blank=False)
     weld_bead_number=models.CharField(verbose_name=u'焊缝编号',max_length=20,blank=False)
-    weld_material_number=models.CharField(verbose_name=u'焊材标号',max_length=20,blank=False)
     model=models.CharField(verbose_name=u'型号',max_length=20,blank=False)
     standard=models.CharField(verbose_name=u'规格',max_length=20,blank=False)
     apply_weight=models.FloatField(verbose_name=u'领用重量',blank=False)
