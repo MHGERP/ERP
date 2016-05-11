37418dc13dd7c8ab8434180f51e1f7923f7f9525
chousan1989 <347096491@qq.com>
2016-05-12 05:35:39 +0800

    qualitycard

---
+++ media/js/purchasing/quality_price_card.js
@@ -0,0 +1,24 @@
+
+$("#qualitycard_confirm").click(function(){
+    var form=$("#quality_card_form");
+    var quality_card_id=$("#quality_card_div").attr("qualitycardid");
+    supplier_form_set=Array();
+    supplier_id_set=Array();
+    $(".supplierform").each(function(){
+        supplier_form_set.push($(this).serialize(true));
+        supplier_id_set.push($(this).attr("supplierselect"));
+    });
+    Dajaxice.purchasing.saveQualityCard(function(data){
+        if(data.status ==0 ){
+            window.location.reload();
+        }
+        else{
+            alert("表单填写有误");
+        }
+    },{
+        'form':$(form).serialize(true),
+        'quality_card_id':quality_card_id,
+        'supplier_form_set':supplier_form_set,
+        'supplier_id_set':supplier_id_set
+    });
+});
