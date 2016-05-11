493f126e739f00cf0ad40e1c834d7ef9ed622f4e
obelisk_the_mxl <mxl.obelisk@hotmail.com>
2016-05-11 18:15:03 +0800

    sell file up

--- sell/views.py
+++ sell/views.py
@@ -33,7 +33,6 @@ def product_bidFile_add(request):
             bidfile.file_size = str(int(request.FILES['product_file'].size) / 1000) + "kb"
             bidfile.name = request.FILES['product_file'].name
             bidfile.upload_date = datetime.datetime.now()
-            bidfile.file_type = False
             bidfile.save()
             if group_type == "manufacture":
                 group = Group.objects.filter(name__icontains = u"生产")
@@ -44,6 +43,38 @@ def product_bidFile_add(request):
             else:
                 group = Group.objects.filter(name__icontains = u"采购")
                 product.purchasing_file_down = bidfile
+            bidfile.recv_group = group[0] if len(group) > 0 else None
+            bidfile.save()
+            product.save()
+            file_upload_error = 1
+        return HttpResponse(json.dumps({"file_upload_error" : file_upload_error, }))
+
+
+def product_bidFile_back(request):
+    if request.is_ajax():
+        if request.FILES['product_file'].size > 10 * 1024 * 1024:
+            file_upload_error = 2
+        else:
+            product_id = request.POST['product_id']
+            group_type = request.POST['group_type']
+            #print product_id
+            #print group_type
+            product = Product.objects.get(id = product_id)
+            bidfile = BidFile()
+            bidfile.file_obj = request.FILES['product_file']
+            bidfile.file_size = str(int(request.FILES['product_file'].size) / 1000) + "kb"
+            bidfile.name = request.FILES['product_file'].name
+            bidfile.upload_date = datetime.datetime.now()
+            bidfile.save()
+            if group_type == "manufacture":
+                group = Group.objects.filter(name__icontains = u"生产")
+                product.manufacture_file_up = bidfile
+            elif group_type == "techdata":
+                group = Group.objects.filter(name__icontains = u"工艺")
+                product.techdata_file_up = bidfile
+            else:
+                group = Group.objects.filter(name__icontains = u"采购")
+                product.purchasing_file_up = bidfile
             bidfile.recv_group = group[0] if len(group) > 0 else None
             bidfile.save()
             product.save()
